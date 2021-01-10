d2979ece9a178a0affbfb16beb359f383d90085c
tichnas <sanchit.arora.2002@gmail.com>
2021-01-10 13:11:53 +0530

    Database connection and Server setup

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const connectDB = require('./utils/connectDB');
+
+const app = express();
+
+connectDB();
+
+app.use(express.json());
+
+app.get('/', (req, res) => res.send('API running'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log('Server Started'));
